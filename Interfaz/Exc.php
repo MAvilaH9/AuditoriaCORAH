579be7bf801f8ac2d3608d368b7940061ff36a7e
Mario <mavilah9@gmail.com>
2019-06-24 08:24:39 -0500

    Se agregó PHPExcel y modifico a la descarga de archivos a xlsx

---
+++ Interfaz/Exc.php
@@ -0,0 +1,121 @@
+<?php include "Templete/Header.php"; ?>
+
+<!-- Static Table Start -->
+<div class="data-table-area mg-b-15">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="sparkline13-list">
+                    <div class="sparkline13-hd">
+                        <div class="main-sparkline13-hd" style="text-align: center">
+                            <h1>Subir <span class="table-project-n">archivo</span> de reporte</h1>
+                            <ul class="breadcome-menu">
+                                <li><a href="Index.php" data-toggle="tooltip" title="Regresar al inicio">Inicio</a>
+                                    <span class="bread-slash">/</span>
+                                </li>
+                                <li><span class="bread-blod">Reportes</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div> <br> <br>
+                    <!-- Formulario -->
+                    <form id="frmExcel">
+                        <div class="form-group-inner">
+                            <div class="row">
+                                <div class="col-lg-3 col-md-12 col-sm-12 col-xs-12">
+                                    <label class="login2 pull-right pull-right-pro">Agregar Archivo Excel</label>
+                                </div>
+                                <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+                                    <div class="file-upload-inner ts-forms">
+                                        <div class="input prepend-big-btn">
+                                            <label class="icon-right" for="prepend-big-btn">
+                                                <i class="fa fa-download"></i>
+                                            </label>
+                                            <div class="file-button">
+                                                Agregar
+                                                <input type="file" name="Archivo"
+                                                    onchange="document.getElementById('prepend-big-btn').value = this.value;"
+                                                    accept=".xlsx">
+                                            </div>
+                                            <input type="text" id="prepend-big-btn"
+                                                placeholder="No se ha seleccionado ningun archivo">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-2 col-md-12 col-sm-12 col-xs-12">
+                                    <button type="submit" name="action" id="action" value="Add"
+                                        class="btn btn-custon-rounded-two btn-success">
+                                        <i class="fa fa-check edu-checked-pro" aria-hidden="true"></i>
+                                        Guardar
+                                    </button>
+                                </div>
+                            </div> <br> <br>
+                            <div class="row">
+                                <div class="col-lg-3 col-md-12 col-sm-12 col-xs-12">
+                                </div>
+                                <!-- Alertas  -->
+                                <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+                                    <div id="AlertExito" class="alert alert-success" role="alert" style="display:none">
+                                        <strong>Exitoso!</strong> Archivo guardados con exito
+                                    </div>
+                                    <div id="AlertError" class="alert alert-danger alert-mg-b" style="display:none">
+                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                        <strong>Error!</strong> Archivo no guardado.
+                                    </div>
+                                    <div id="AlertArcExistente" class="alert alert-danger alert-mg-b" style="display:none">
+                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                        <strong>Advertencia!</strong> Archivo ya existe.
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-md-12 col-sm-12 col-xs-12">
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div> <br>
+
+    <?php include "Templete/Footer.php"; ?>
+
+    <script>
+        // Agregar
+        $(document).on('submit', '#frmExcel', function (event) {
+            event.preventDefault();
+            var datos = $('#frmExcel').serialize();
+            alert(datos);
+            $.ajax({
+                url: "../Recursos/pros.php",
+                method: 'POST',
+                data: new FormData(this),
+                contentType: false,
+                processData: false,
+                success: function (data) {
+                    alert(data);
+                    $('#frmExcel')[0].reset();
+                    if (data == 1) {
+                        $("#AlertExito").fadeIn();
+                        setTimeout(function () {
+                            $("#AlertExito").fadeOut();
+                        }, 2000);
+                    } else if (data == 2) {
+                        $("#AlertError").fadeIn();
+                        setTimeout(function () {
+                            $("#AlertError").fadeOut();
+                        }, 2000);
+                    }else{
+                        $("#AlertArcExistente").fadeIn();
+                        setTimeout(function () {
+                            $("#AlertArcExistente").fadeOut();
+                        }, 2000);
+                    }
+                }
+            });
+        });
+        
+    </script>
--- Recursos/pros.php
+++ Recursos/pros.php
@@ -1,45 +1,53 @@
+
 <?php
-//obtenemos el archivo .csv
-$tipo = $_FILES['archivo']['type'];
 
-$tamanio = $_FILES['archivo']['size'];
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 
-$archivotmp = $_FILES['archivo']['tmp_name'];
+    require_once "../SQLServer/Conexion.php";
+    $fecha= date("d/m/Y");
+    $Sucursal=$_GET['Sucursal'];
+    $us=$_SESSION['Usuario'];
 
-//cargamos el archivo
-$lineas = file($archivotmp);
+    $sqlsuc = $pdo->prepare("SELECT Nombre FROM Sucursal where Sucursal=$Sucursal");
+    $sqlsuc -> execute(array($Sucursal));
+    $resultadosuc = $sqlsuc->fetch();
+    $NombSuc=$resultadosuc['Nombre'];
 
-//inicializamos variable a 0, esto nos ayudará a indicarle que no lea la primera línea
-$i=0;
+    $sql= $pdo->prepare("SELECT * FROM Articulo WHERE Sucursal='$Sucursal' ORDER BY Articulo ASC");
+    $sql->execute(array(':kind' => 'drupe'));
 
-//Recorremos el bucle para leer línea por línea
-foreach ($lineas as $linea_num => $linea)
-{ 
-   //abrimos bucle
-   /*si es diferente a 0 significa que no se encuentra en la primera línea 
-   (con los títulos de las columnas) y por lo tanto puede leerla*/
-   if($i != 0) 
-   { 
-       //abrimos condición, solo entrará en la condición a partir de la segunda pasada del bucle.
-       /* La funcion explode nos ayuda a delimitar los campos, por lo tanto irá 
-       leyendo hasta que encuentre un ; */
-       $datos = explode(";",$linea);
-   
-       //Almacenamos los datos que vamos leyendo en una variable
-       //usamos la función utf8_encode para leer correctamente los caracteres especiales
-       $nombre = utf8_encode($datos[0]);
-       $edad = $datos[1];
-       $profesion = utf8_encode($datos[2]);
+ if ($resultado > 0) {
+   require_once '../ClasesExcel/PHPExcel.php';
+   $objPHPExcel = new PHPExcel();
 
-       //guardamos en base de datos la línea leida
-       mysql_query("INSERT INTO datos(nombre,edad,profesion) VALUES('$nombre','$edad','$profesion')");
+   //Informacion del excel
+   $objPHPExcel->
+    getProperties()
+        ->setCreator("lahuerta")
+        ->setLastModifiedBy("lahuerta")
+        ->setTitle("Exportar Base de Datos")
+        ->setSubject("Tabla")
+        ->setDescription("Documento generado con PHPExcel")
+        ->setKeywords("lahuerta  con  phpexcel")
+        ->setCategory("solicitudes");    
 
-       //cerramos condición
+   $i = 1;    
+   while ($registro = $sql->fetch(PDO::FETCH_ASSOC)) {
+
+      $objPHPExcel->setActiveSheetIndex(0)
+            ->setCellValue('A'.$i, $registro->name);
+
+      $i++;
+
    }
-
-   /*Cuando pase la primera pasada se incrementará nuestro valor y a la siguiente pasada ya 
-   entraremos en la condición, de esta manera conseguimos que no lea la primera línea.*/
-   $i++;
-   //cerramos bucle
 }
+    header('Content-Type: application/vnd.ms-excel');
+    header('Content-Disposition: attachment;filename="tabla.xlsx"');
+    header('Cache-Control: max-age=0');
+
+    $objWriter=PHPExcel_IOFactory::createWriter($objPHPExcel,'Excel2007');
+    $objWriter->save('php://output');
+    exit;
+    mysql_close ();
 ?>
